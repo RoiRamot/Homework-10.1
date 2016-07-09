24354df3a8d2bf1677517175afc417195d6d2d4e
roiramot <roir@codevalue.net>
2016-07-09 21:00:04 +0300

    Add
    Add

---
+++ Personnel/FileReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel
+{
+    class FileReader
+    {
+        public  List<string> Reader(string path)
+        {
+            if (path != null && File.Exists(path))
+            {
+                StreamReader reader = new StreamReader(path);
+                List<string> list = new List<string>();
+                string name;
+                while ((name = reader.ReadLine()) != null)
+                {
+                    if (name != "")
+                    {
+                        list.Add(name);
+                    }
+
+                }
+                reader.Close();
+                return list;
+            }
+            return null;
+        }
+    }
+}
---
+++ Personnel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\projects\קורס .net\10.1\Personnel\NamesList.txt";
+            FileReader reader = new FileReader();
+            if (reader.Reader(path) == null)
+            {
+                Console.WriteLine("file path is incorrect");
+            }
+            else
+            {
+                foreach (string line in reader.Reader(path))
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
